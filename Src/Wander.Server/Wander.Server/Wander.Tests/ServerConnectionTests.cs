ef3f6d709af8ca3584f46f9f06e2ddb7073d741e
val92130 <chatelain@intechinfo.fr>
2015-10-21 13:38:00 +0200

    Simple test

---
+++ Src/Wander.Server/Wander.Server/Wander.Tests/ServerConnectionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+
+namespace Wander.Server.Wander.Tests
+{
+    [TestFixture]
+    public class ServerConnectionTests
+    {
+        [Test]
+        public void TestClientConnectionWorks()
+        {
+            // To implement
+
+            Assert.True(1==1);
+        }
+    }
+}
