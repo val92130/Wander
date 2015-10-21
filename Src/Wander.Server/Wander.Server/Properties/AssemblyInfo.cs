832fad45c8c8706e48337c27f8bfbb96fa2e01cd
val92130 <chatelain@intechinfo.fr>
2015-10-21 13:08:04 +0200

    New server

--- Src/Wander.Server/Wander.Server/Properties/AssemblyInfo.cs
+++ Src/Wander.Server/Wander.Server/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7bf827a3-fdf7-4328-a994-e15962820e31")]
+[assembly: Guid("2847b8dc-ce6b-46a0-9d70-e5244ecf0661")]
 
 // Version information for an assembly consists of the following four values:
 //
