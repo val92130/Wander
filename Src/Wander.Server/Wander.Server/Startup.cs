7164a2148479d6ec9e2cd5439d06f0a4e2a87b8e
val92130 <chatelain@intechinfo.fr>
2015-10-23 15:13:01 +0200

    SignalR startup class
    TypeScript client script

---
+++ Src/Wander.Server/Wander.Server/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Wander.Server.Startup))]
+
+namespace Wander.Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
