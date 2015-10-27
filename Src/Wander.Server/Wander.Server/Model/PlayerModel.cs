a91b4f33511701f3cf689f10e800526b8a1109e3
val92130 <chatelain@intechinfo.fr>
2015-10-27 14:29:31 +0100

    PlayerModel class

---
+++ Src/Wander.Server/Wander.Server/Model/PlayerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wander.Server.Model
+{
+    public class PlayerModel
+    {
+        public string UserId { get; set; }
+        public string SignalRId { get; set; }
+    }
+}
