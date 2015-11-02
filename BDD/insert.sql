0c14f5248f52c51f1bfb2c5673ebbcf40f8cf937
VanPalamar <rami.morri@gmail.com>
2015-11-02 16:42:53 +0100

    insert example

--- BDD/insert.sql
+++ BDD/insert.sql
@@ -3,3 +3,16 @@ INSERT INTO dbo.ListProperties (NameProperty, PropertyDescription, Threshold, Pr
 INSERT INTO dbo.ListProperties (NameProperty, PropertyDescription, Threshold, Price) values ('test3', 'description3', 2, 150);
 INSERT INTO dbo.ListProperties (NameProperty, PropertyDescription, Threshold, Price) values ('test4', 'description4', 2, 3000);
 INSERT INTO dbo.ListProperties (NameProperty, PropertyDescription, Threshold, Price) values ('test5', 'description5', 2, 250);
+
+INSERT INTO dbo.Users(UserLogin, UserPassword, Email, Sex, Account, Points, Connected, Activated, JobId) values ('testLogin', 'test','test @gmail.com',1,0,0,1,0,0);
+
+INSERT INTO dbo.ListProperties (NameProperty, PropertyDescription, Threshold, Price) values ('test5', 'description5', 2, 250);
+
+SELECT l.ListPropertyId, l.NameProperty, l.PropertyDescription, l.Threshold, l.Price
+from dbo.UserProperties p
+JOIN dbo.Users u on p.UserId = u.UserId
+JOIN dbo.ListProperties l ON l.ListPropertyId = p.UserPropertyId
+WHERE u.UserId = 0
+
+INSERT INTO dbo.UserProperties (UserId, ListPropertyId) values (0, 0);
+SELECT COUNT (u.ListPropertyId) as count, l.Threshold FROM dbo.ListProperties l JOIN UserProperties u ON u.ListPropertyId = l.ListPropertyId WHERE u.ListPropertyId = 0 GROUP BY Threshold;
