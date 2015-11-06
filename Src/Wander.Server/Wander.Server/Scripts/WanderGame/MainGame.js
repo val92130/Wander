24ae44fbb90463e12fc88c246b3a350ea046570c
val92130 <chatelain@intechinfo.fr>
2015-11-06 16:31:11 +0100

    Js update

---
+++ Src/Wander.Server/Wander.Server/Scripts/WanderGame/MainGame.js
@@ -0,0 +1,36 @@
+var WanderGame = (function () {
+    function WanderGame() {
+        this.game = new Phaser.Game(640, 480, Phaser.AUTO, 'main', {
+            create: this.create, preload: this.preload, render: this.render, update: this.update
+        });
+    }
+    WanderGame.prototype.preload = function () {
+        this.game.load.tilemap("ItsTheMap", "Content/Game/Maps/newMap2.json", null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image("Tiles", "Content/Game/Images/breadinator_tileset.png");
+    };
+    WanderGame.prototype.render = function () {
+    };
+    WanderGame.prototype.create = function () {
+        this.map = this.game.add.tilemap("ItsTheMap");
+        this.map.addTilesetImage("tileset", "Tiles");
+        this.map.createLayer("Calque de Tile 1").resizeWorld();
+        this.game.camera.setSize(600, 200);
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+    };
+    WanderGame.prototype.update = function () {
+        if (this.cursors.up.isDown) {
+            this.game.camera.y -= 4;
+        }
+        else if (this.cursors.down.isDown) {
+            this.game.camera.y += 4;
+        }
+        if (this.cursors.left.isDown) {
+            this.game.camera.x -= 4;
+        }
+        else if (this.cursors.right.isDown) {
+            this.game.camera.x += 4;
+        }
+        this.game.debug.cameraInfo(this.game.camera, 32, 32);
+    };
+    return WanderGame;
+})();
