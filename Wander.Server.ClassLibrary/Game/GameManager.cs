0894a8f8ab24e75472eeaba52b03e75cf33ce4ec
val92130 <chatelain@intechinfo.fr>
2016-01-22 17:52:44 +0100

    Json map parser

--- Wander.Server.ClassLibrary/Game/GameManager.cs
+++ Wander.Server.ClassLibrary/Game/GameManager.cs
@@ -11,6 +11,7 @@ using Wander.Server.ClassLibrary.Services;
 using System.Threading;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections;
 
 namespace Wander.Server.ClassLibrary
 {
@@ -23,6 +24,8 @@ namespace Wander.Server.ClassLibrary
         System.Timers.Timer _randomRainTimer = new System.Timers.Timer();
         public static int DefaultUnemployedEarningPoints = 2;
         bool _isRaining = false;
+        private int _mapWidth, _mapHeight;
+        private byte[] _map;
         public GameManager()
         {
             LoadMap();
@@ -56,7 +59,55 @@ namespace Wander.Server.ClassLibrary
             using (var wc = new System.Net.WebClient())
                 contents = wc.DownloadString("http://wander.nightlydev.fr/Content/Game/Maps/map2.json");
             JObject map = JsonConvert.DeserializeObject<JObject>(contents);
+            this._mapWidth = Convert.ToInt32(map["width"].ToString());
+            this._mapHeight = Convert.ToInt32(map["width"].ToString());
+            _map = new byte[_mapWidth * _mapHeight];
+            var layers = map["layers"];
+            foreach (var i in layers)
+            {
+                string layerName = i["name"].ToString();
+                Debug.Print(layerName);
+                var layerData = i["data"];
 
+
+
+                if (layerName == "backgroundLayer")
+                {
+                    int counter = 0;
+                    layerData.ForEach(x =>
+                    {
+                        _map[counter] = 0;
+                        counter++;
+
+                    });
+                }
+
+                else if (layerName == "collisionLayer")
+                {
+                    int counter = 0;
+                    layerData.ForEach(x =>
+                    {
+                        if ((long)(x) != 0)
+                        {
+                            _map[counter] = 1;
+                        }
+
+                        counter++;
+
+                    });
+
+                }
+            }
+
+        }
+
+        public bool IsCollision(int x, int y)
+        {
+            if (x < 0 || x >= this._mapWidth || y < 0 || y >= this._mapHeight)
+            {
+                return true;
+            }
+            return _map[y*this._mapHeight + x] == 1;
         }
 
 
